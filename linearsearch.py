17b4b62c5fc3a49bcb4fe8269a5f3b6c0d24b448
Mani Rohith <82700071+itsMMR@users.noreply.github.com>
2021-07-21 12:39:00 +0530

    Update linearsearch.py

--- linearsearch.py
+++ linearsearch.py
@@ -1,1 +1,15 @@
+a=int(input("enter length of the list"))
+l=[]
+for i in range(a):
+    l=l+[int(input("enter any number"))]
+print(l)
+key=int(input("enter any number to search"))
+flag=0
+for i in range(a):
+    if l[i]==key:
+        print("Elements is found at",i)
+        flag=1
+        break
+if flag==0:
+    print("Element is not found in list")
 
